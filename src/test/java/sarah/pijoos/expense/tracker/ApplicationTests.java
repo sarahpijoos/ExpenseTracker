f0ea66436c8862ae29c8d7bdc5094bdc9ae4df40
sarahpijoos <sarahpijoos@gmail.com>
2024-02-07 14:35:12 +0200

    feat(Setup): added test for initial endpoint

--- src/test/java/sarah/pijoos/expense/tracker/ApplicationTests.java
+++ src/test/java/sarah/pijoos/expense/tracker/ApplicationTests.java
@@ -6,8 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ApplicationTests {
 
+	//will fail if the application context cannot star
 	@Test
 	void contextLoads() {
 	}
+
+
 
 }
