14e21681a83abd61af3de17d16806692cffd59c3
sarahpijoos <sarahpijoos@gmail.com>
2024-03-17 12:41:11 +0200

    chore: Argon2 password hashing

--- src/test/java/sarah/pijoos/expense/tracker/ApplicationTests.java
+++ src/test/java/sarah/pijoos/expense/tracker/ApplicationTests.java
@@ -2,6 +2,9 @@ package sarah.pijoos.expense.tracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class ApplicationTests {
@@ -9,4 +12,14 @@ class ApplicationTests {
   // will fail if the application context cannot star
   @Test
   void contextLoads() {}
+
+  @Test
+  public void givenRawPassword_whenEncodedWithArgon2_thenMatchesEncodedPassword() {
+    String rawPassword = "Baeldung";
+    Argon2PasswordEncoder arg2SpringSecurity = new Argon2PasswordEncoder(16, 32, 1, 60000, 10);
+    String springBouncyHash = arg2SpringSecurity.encode(rawPassword);
+
+    boolean isValid = arg2SpringSecurity.matches(rawPassword, springBouncyHash);
+    assertTrue(isValid);
+  }
 }
